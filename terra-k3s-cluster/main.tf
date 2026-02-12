a7f71bd26c43ed7101f94d331181422f5018aa51
Ubuntu <ubuntu@ip-172-31-1-151.ap-south-1.compute.internal>
2026-02-12 05:56:51 +0000

    Updated main.tf for port 30007

--- terra-k3s-cluster/main.tf
+++ terra-k3s-cluster/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# ---------------- SECURITY GROUP ----------------
 resource "aws_security_group" "k3s_sg" {
   name = "k3s-sg"
 
@@ -25,6 +26,14 @@ resource "aws_security_group" "k3s_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port   = 0
@@ -34,6 +43,7 @@ resource "aws_security_group" "k3s_sg" {
   }
 }
 
+# ---------------- EC2 INSTANCE ----------------
 resource "aws_instance" "k3s" {
   ami                         = "ami-02b8269d5e85954ef"
   instance_type               = "t3.medium"
@@ -43,18 +53,25 @@ resource "aws_instance" "k3s" {
   vpc_security_group_ids      = [aws_security_group.k3s_sg.id]
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y curl",
+  inline = [
+    "sudo apt update -y",
+    "sudo apt install -y curl",
 
-      # Install K3s with Public IP as TLS SAN
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"--tls-san ${self.public_ip}\" sh -",
+    # Install K3s with Public IP as TLS SAN
+    "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"--tls-san ${self.public_ip}\" sh -",
 
-      "sudo mkdir -p /home/ubuntu/.kube",
-      "sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config",
-      "sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config"
-    ]
+    "sudo mkdir -p /home/ubuntu/.kube",
+    "sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config",
+    "sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config",
+    "sudo chmod 700 /home/ubuntu/.kube/config",
+	
+	# Prepare kubeconfig
+    "sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/k3s.yaml",
+    "sudo sed -i 's/127.0.0.1/${self.public_ip}/g' /home/ubuntu/k3s.yaml",
+    "sudo base64 -w 0 /home/ubuntu/k3s.yaml > /home/ubuntu/k3s.yaml.b64"
+   ]
   }
+
 
   connection {
     type        = "ssh"
@@ -66,4 +83,10 @@ resource "aws_instance" "k3s" {
   tags = {
     Name = "k3s-server"
   }
+}
+
+
+# ---------------- OUTPUT BASE64 ----------------
+output "k3s_kubeconfig_base64" {
+  value = file("${path.module}/kubeconfig.b64")
 }
